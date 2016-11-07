70308904057b5f872f6d5d81e671caa8a313742e
Katy310 <katyayani15046@iiitd.ac.in>
2016-11-07 23:06:15 +0530

    Modified GUI Class

--- Project/src/GUI.java
+++ Project/src/GUI.java
@@ -1,4 +1,21 @@
 
 public class GUI {
+	...//Container pane = aFrame.getContentPane()...
+	JButton button = new JButton("Button 1 (PAGE_START)");
+	pane.add(button, BorderLayout.PAGE_START);
 
+	//Make the center component big, since that's the
+	//typical usage of BorderLayout.
+	button = new JButton("Button 2 (CENTER)");
+	button.setPreferredSize(new Dimension(200, 100));
+	pane.add(button, BorderLayout.CENTER);
+
+	button = new JButton("Button 3 (LINE_START)");
+	pane.add(button, BorderLayout.LINE_START);
+
+	button = new JButton("Long-Named Button 4 (PAGE_END)");
+	pane.add(button, BorderLayout.PAGE_END);
+
+	button = new JButton("5 (LINE_END)");
+	pane.add(button, BorderLayout.LINE_END);
 }
