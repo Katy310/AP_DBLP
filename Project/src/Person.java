8a5fcb59a2aa3790a1e93db9fc31f848bed7e340
khurranasagar <sagar15167@iiitd.ac.in>
2016-11-08 22:43:54 +0530

    Basic Person Code Created

--- Project/src/Person.java
+++ Project/src/Person.java
@@ -1,9 +1,33 @@
-//import java.util.ArrayList;
+import java.util.ArrayList;
 
 public class Person {
 
-	//private String primName;
-	//private ArrayList<String> names = new ArrayList<String>();
-	//private int noPubl;
+	private String PrimName;
+	private ArrayList<String> names = new ArrayList<String>();
+	private int NoPubl;
+	
+	public void setPrimName(String primName){
+		this.PrimName = primName;
+	}
+	
+	public void publFound(){
+		NoPubl++;
+	}
+	
+	public void addName(String name){
+		names.add(name);
+	}
+	
+	public String getPrimName(){
+		return(PrimName);
+	}
+	
+	public int getNoOfPubl(){
+		return(NoPubl);
+	}
+	
+	public boolean ifSame(String name){
+		return(names.contains(name));
+	}
 	
 }
