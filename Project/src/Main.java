900b605d9eb3d611c73d5db92d214f536997f046
Katy310 <katyayani15046@iiitd.ac.in>
2016-11-09 23:47:11 +0530

    Modified Main.java

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -2,24 +2,25 @@ import java.util.ArrayList;
 
 public class Main {
 
-	private static ArrayList<Publication> publications = new ArrayList<Publication>(); 
+	private static ArrayList<Publication> publications = new ArrayList<Publication>();
 	//private static ArrayList<Person> persons = new ArrayList<Person>();
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Parser pars = new Parser();
 		pars.parseAndSave();
 		showAllPublications();
 	}
-	
+
 	public static void addPublications(Publication pub){
 		publications.add(pub);
 	}
-	
+
 	public static void showAllPublications(){
 		for(Publication pub:publications){
 			System.out.println(pub.toString());
 		}
 	}
 
+    int i = 0;
 }
