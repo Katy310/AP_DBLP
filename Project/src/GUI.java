255152371c072fa79cd6d213f6ad952e9e071ff5
Katy310 <katyayani15046@iiitd.ac.in>
2016-11-11 20:12:50 +0530

    Cleaned code

--- Project/src/GUI.java
+++ Project/src/GUI.java
@@ -1,42 +1,39 @@
-import java.awt.*;
 import javax.swing.*;
-
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random; 
 
 public class GUI {
 	
-	public static JLabel getLabel(String text) 
+	private JFrame frame;
+	private JPanel panelHead;
+	private JLabel labelHead;
+	private JPanel mainLeftPanel;
+	private BoxLayout boxLayout;
+	private JComboBox<String> queryList;
+
+	public void GUI ()
 	{
-        return getLabel(text, SwingConstants.LEFT);
-    }
+		frame = new JFrame();
 
-    
-    public static JLabel getLabel(String text, int alignment) 
-    {
-        JLabel l = new JLabel(text, alignment);
-        return l;
-    }
-
-
-	public static void main(String[] args) 
-	{
-		JFrame frame = new JFrame();
-
-		JPanel panelHead = new JPanel(new BorderLayout());
-		JLabel labelHead = getLabel("DBLP Query Engine", SwingConstants.CENTER);
+		panelHead = new JPanel(new BorderLayout());
+		labelHead = getLabel("DBLP Query Engine", SwingConstants.CENTER);
 		labelHead.setFont(new Font("Serif", Font.BOLD, 24));
 		panelHead.add(labelHead, BorderLayout.CENTER);
 
-		JPanel mainLeftPanel = new JPanel();
+		mainLeftPanel = new JPanel();
 		mainLeftPanel.setMinimumSize(new Dimension(30,30));
 		mainLeftPanel.setPreferredSize(new Dimension(200,600));
 		
-		BoxLayout boxLayout = new BoxLayout(mainLeftPanel, BoxLayout.Y_AXIS);
+		boxLayout = new BoxLayout(mainLeftPanel, BoxLayout.Y_AXIS);
 		
 		String[] queries = { "Queries", "Query 1", "Query 2"};
-		JComboBox<String> queryList = new JComboBox<String>(queries);
+		queryList = new JComboBox<String>(queries);
 		queryList.setSelectedIndex(0);
-		mainLeftPanel.add(Box.createRigidArea(new Dimension(0,100)));
+		mainLeftPanel.add(Box.createRigidArea(new Dimension(0,50)));
 		mainLeftPanel.add(queryList);
+		
+		queryList.addActionListener(new queryListener());
 		
 		JPanel mainRightPanel = new JPanel();
 		mainRightPanel.setMinimumSize(new Dimension(30,30));
@@ -58,6 +55,42 @@ public class GUI {
 		frame.getContentPane().add(mainFrame);
 		frame.setSize(600,600);
 		frame.setVisible(true);
+	}
+	
+	class queryListener implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			JComboBox cb = (JComboBox)e.getSource();
+		    String query = (String)cb.getSelectedItem();
+			if (query.equals("Query 1"))
+			{
+			    panelHead.setVisible(false);
+			}
+			else
+			{
+			    mainLeftPanel.setVisible(false);
+			}
+		}
+	}
+
+	public static JLabel getLabel(String text) 
+	{
+        return getLabel(text, SwingConstants.LEFT);
+    }
+
+    
+    public static JLabel getLabel(String text, int alignment) 
+    {
+        JLabel l = new JLabel(text, alignment);
+        return l;
+    }
+
+
+	public static void main(String[] args) 
+	{
+		GUI main = new GUI();
 	}
 	
 }
