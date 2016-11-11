3c2f4ed3fe29db996668c6815d4414e94a06321e
Katy310 <katyayani15046@iiitd.ac.in>
2016-11-10 21:38:53 -0800

    Added first main frame

--- Project/src/GUI.java
+++ Project/src/GUI.java
@@ -1,32 +1,49 @@
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-
+import java.awt.*;
 import javax.swing.*;
 
 
 public class GUI {
 	
-	public static void main(String[] args) {
+	public static JLabel getLabel(String text) {
+        return getLabel(text, SwingConstants.LEFT);
+    }
+
+    
+    public static JLabel getLabel(String text, int alignment) {
+        JLabel l = new JLabel(text, alignment);
+        return l;
+    }
+    
+	
+	public static void main(String[] args) 
+	{
 		JFrame frame = new JFrame();
-		//Container pane = aFrame.getContentPane()...
-		JButton button = new JButton("Button 1 (PAGE_START)");
-		frame.getContentPane().add(button, BorderLayout.PAGE_START);
 
-		//Make the center component big, since that's the
-		//typical usage of BorderLayout.
-		button = new JButton("Button 2 (CENTER)");
-		button.setPreferredSize(new Dimension(200, 100));
-		frame.getContentPane().add(button, BorderLayout.CENTER);
+		JPanel panelHead = new JPanel(new BorderLayout());
+        panelHead.add(getLabel("DBLP Query Engine", SwingConstants.CENTER), BorderLayout.CENTER);
 
-		button = new JButton("Button 3 (LINE_START)");
-		frame.getContentPane().add(button, BorderLayout.LINE_START);
-
-		button = new JButton("Long-Named Button 4 (PAGE_END)");
-		frame.getContentPane().add(button, BorderLayout.PAGE_END);
-
-		button = new JButton("5 (LINE_END)");
-		frame.getContentPane().add(button, BorderLayout.LINE_END);
+		JPanel mainLeftPanel = new JPanel();
+		mainLeftPanel.setMinimumSize(new Dimension(30,30));
+		mainLeftPanel.setPreferredSize(new Dimension(200,600));
 		
+		JPanel mainRightPanel = new JPanel();
+		mainRightPanel.setMinimumSize(new Dimension(30,30));
+		mainRightPanel.setPreferredSize(new Dimension(400,600));
+
+
+		JSplitPane mainContent = new JSplitPane( JSplitPane.HORIZONTAL_SPLIT, mainLeftPanel, mainRightPanel);
+		mainContent.setResizeWeight(0.5);
+        mainContent.setOneTouchExpandable(true);
+        mainContent.setContinuousLayout(true);
+        mainContent.setDividerLocation(200 + mainContent.getInsets().left);
+
+		JSplitPane mainFrame = new JSplitPane( JSplitPane.VERTICAL_SPLIT, panelHead, mainContent);
+		mainFrame.setResizeWeight(0.5);
+        mainFrame.setOneTouchExpandable(true);
+        mainFrame.setContinuousLayout(true);
+		mainFrame.setDividerLocation(50 + mainFrame.getInsets().left);
+
+		frame.getContentPane().add(mainFrame);
 		frame.setSize(600,600);
 		frame.setVisible(true);
 	}
