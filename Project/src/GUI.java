9e26eca0e5943221bfe92ca025f21e6e6b2249b8
Katy310 <katyayani15046@iiitd.ac.in>
2016-11-24 22:44:43 +0530

    modified GUI.java

--- Project/src/GUI.java
+++ Project/src/GUI.java
@@ -436,36 +436,3 @@ public class GUI {
 
 }
 
-class MyTableModel extends AbstractTableModel {
-    private String[] columnNames = {"s_no","authors","title","pages","year","volume","journal/Booktitle","url"};
-
-    private Object[][] data;
-
-    public int getColumnCount() {
-        return columnNames.length;
-    }
-
-    public int getRowCount() {
-        return data.length;
-    }
-
-    public String getColumnName(int col) {
-        return columnNames[col];
-    }
-
-    public Object getValueAt(int row, int col) {
-        return data[row][col];
-    }
-
-    public Class getColumnClass(int c) {
-        return getValueAt(0, c).getClass();
-    }
-
-    /*
-     * Don't need to implement this method unless your table's
-     * data can change.
-     */
-    public void setValueAt(Object value, int row, int col) {
-        data[row][col] = value;
-        fireTableCellUpdated(row, col);
-    }
